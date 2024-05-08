3d348b8affb522f94f1a3acbba512bd272bc4451
Lalon Sheikh <you@example.com>
2024-05-09 05:39:51 +0600

    flag added

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,38 @@
+import { useState } from "react";
 import "./Country.css"
+import "../Countries/Countries.css"
+import CountryDetail from "../CountryDetail/CountryDetail";
 
-const Country = ({ country }) => {
-    console.log(country);
-    const { name, flags,population,area } = country;
+const Country = ({ country, handleVisitedCountry, handleVisitedFlags }) => {
+    // console.log(country);
+    const { name, flags, population, area, cca3 } = country;
+
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
+    // console.log(handleVisitedCountry)
     return (
-        <div className="country">
-            <h2>Name : {name.common}</h2>
+        <div className={`country ${visited ? 'visited' : 'non-visited'}`}>
+            <h2 style={{ color: visited ? 'Purple' : 'black' }}>Name : {name.common}</h2>
             <img src={flags.png} alt="" />
             <p>Population : {population}</p>
             <p>Area : {area}</p>
-        </div>
+            <p><small>Code : {cca3}</small></p>
+            <button onClick={() => handleVisitedCountry(country)}>Mark Visited</button> <br />
+            <button onClick={()=>handleVisitedFlags(country.flags.png)}>Add Flags</button>
+            <button onClick={handleVisited}>{visited ? 'visited' : 'Going'}</button>
+            {visited ? 'I have visited this country.' : 'I want to visit.'} <hr />
+            {
+                <CountryDetail
+                country={country}
+                handleVisitedCountry={handleVisitedCountry}
+                handleVisitedFlags={handleVisitedFlags}
+                ></CountryDetail>
+            }
+        </div >
     );
 };
 
---
+++ src/components/CountryData/CountryData.jsx
@@ -0,0 +1,11 @@
+
+
+const CountryData = ({country,handleVisitedCountry,handleVisitedFlags}) => {
+    return (
+        <div>
+            <h4><small>Country Data : {country.name.common}</small></h4>
+        </div>
+    );
+};
+
+export default CountryData;
